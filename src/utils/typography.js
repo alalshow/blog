9cda4b10e1abadafd62fca710377b00021f6d6ed
alalshow <alalshow@hanmail.net>
2019-12-28 20:05:30 +0900

    코드 블럭에 css 적용 완료

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,23 +1,42 @@
+// import Typography from "typography"
+// import Wordpress2016 from "typography-theme-wordpress-2016"
+
+// Wordpress2016.overrideThemeStyles = () => {
+//   return {
+//     "a.gatsby-resp-image-link": {
+//       boxShadow: `none`,
+//     },
+//   }
+// }
+
+// delete Wordpress2016.googleFonts
+
+// const typography = new Typography(Wordpress2016)
+
+// // Hot reload typography in development.
+// if (process.env.NODE_ENV !== `production`) {
+//   typography.injectStyles()
+// }
+
+// export default typography
+// export const rhythm = typography.rhythm
+// export const scale = typography.scale
+
+// import Typography from "typography"
+//import grandViewTheme from "typography-theme-grand-view"
+// import Wordpress2016 from "typography-theme-wordpress-2016"
+
+// const typography = new Typography(Wordpress2016)
+
+// // Export helper functions
+// export const { scale, rhythm, options } = typography
+// export default typography
+
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import grandViewTheme from "typography-theme-grand-view"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
+const typography = new Typography(grandViewTheme)
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
-
-// Hot reload typography in development.
-if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
-}
-
+// Export helper functions
+export const { scale, rhythm, options } = typography
 export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
